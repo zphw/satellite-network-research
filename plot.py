7d48c8281b2a8d5c1c04239c11ffec8ede286c5a
zphw <zph@zph.io>
2021-04-11 23:01:51 +0800

    rwin

--- plot.py
+++ plot.py
@@ -3,7 +3,7 @@ import pandas as pd
 from log import logger
 
 
-class PlotOneProtocol:
+class PlotOne:
     def __init__(self, protocol, csv_file, output_name):
         self.protocol = protocol
         self.csv_file = csv_file
@@ -14,18 +14,18 @@ class PlotOneProtocol:
         self.throughput = []
         self.seconds = []
 
-    def __compute_time_since_start(self):
+    def _compute_time_since_start(self):
         for i in range(len(self.times_raw)):
             td = pd.Timedelta(str(self.times_raw[i] - self.times_raw[0]))
             self.times_since_start.append(td.seconds + (td.microseconds / 1000000) + (td.nanoseconds / 1000000000))
 
-    def __compute_throughput(self):
+    def _compute_throughput(self):
         next_whole_sec = 1
         data_sent = 0
         for i in range(len(self.df['frame.len'])):
             data_sent += self.df['frame.len'][i]
 
-            if self.times_since_start[i] >= float(next_whole_sec):
+            if self.times_since_start[i] > float(next_whole_sec):
                 self.throughput.append((data_sent * 8) / 1000000)
                 self.seconds.append(next_whole_sec)
                 data_sent = 0
@@ -33,8 +33,8 @@ class PlotOneProtocol:
 
     def plot_tput_vs_time(self):
         logger.info("Started plotting...")
-        self.__compute_time_since_start()
-        self.__compute_throughput()
+        self._compute_time_since_start()
+        self._compute_throughput()
 
         plt.plot(self.seconds, self.throughput)
         plt.xlabel("Time (seconds)")
@@ -44,5 +44,64 @@ class PlotOneProtocol:
         filename = f'graphs/{self.output_name}-throughput-vs-time.png'
         plt.savefig(filename)
         logger.info("Plot saved to " + filename)
+
+        plt.show()
+
+
+class PlotComparison:
+    def __init__(self, protocol, legends, csv_files, trial_name):
+        self.protocol = protocol
+        self.legends = legends
+        self.csv_files = csv_files
+        self.trial_name = trial_name
+        self.df = []
+        for csv in csv_files:
+            self.df.append(pd.read_csv(csv))
+
+        self.times_raw = []
+        for data in self.df:
+            self.times_raw.append(pd.to_datetime(data['frame.time'], infer_datetime_format=True))
+
+        self.times_since_start = []
+        self.throughput = []
+        self.seconds = []
+
+    def _compute_time_since_start(self):
+        for i in range(len(self.times_raw[0])):
+            td = pd.Timedelta(str(self.times_raw[0][i] - self.times_raw[0][0]))
+            self.times_since_start.append(td.seconds + (td.microseconds / 1000000) + (td.nanoseconds / 1000000000))
+
+    def _compute_throughput(self):
+        for data in self.df:
+            next_whole_sec = 1
+            data_sent = 0
+            throughput = []
+            seconds = []
+            for i in range(len(data['frame.len'])):
+                data_sent += data['frame.len'][i]
+
+                if self.times_since_start[i] > float(next_whole_sec):
+                    throughput.append((data_sent * 8) / 1000000)
+                    seconds.append(next_whole_sec)
+                    data_sent = 0
+                    next_whole_sec += 1
+
+            self.throughput.append(throughput)
+            self.seconds.append(seconds)
+
+    def plot_tput_vs_time(self):
+        logger.info("Started plotting...")
+        self._compute_time_since_start()
+        self._compute_throughput()
+
+        for i in range(len(self.throughput)):
+            plt.plot(self.seconds[i], self.throughput[i])
+        plt.xlabel("Time (seconds)")
+        plt.ylabel("Throughput (Mbits)")
+        plt.legend(self.legends)
+        plt.title(self.protocol)
+        plot_filename = f'graphs/{self.trial_name}-throughput-vs-time.png'
+        plt.savefig(plot_filename)
+        logger.info("Plot saved to " + plot_filename)
 
         plt.show()
--- trial.py
+++ trial.py
@@ -1,7 +1,7 @@
 import os
 import time
 from log import logger
-from plot import PlotOneProtocol
+from plot import PlotComparison
 from ssh_client import SSHClientOverProxy
 
 
@@ -21,36 +21,44 @@ class Trial:
         self.iperf_server_port = 12222
         self.iperf3_data = iperf_data
         self.iperf3_time = iperf_time
-        self.output_name = f"{protocol}-{int(time.time())}"
+        self.output_names = []
+
+        logger.info("Initializing Trial...")
 
         # init glomma client
-        logger.info("Connecting to glomma...")
         glomma_client = SSHClientOverProxy(username=self.username,
                                            password=self.password,
                                            proxy_host=self.proxy_host,
                                            host=self.client_host)
         self.glomma_ssh = glomma_client.get_client()
 
-    # Trial Procedure Functions Start #
-
-    def __init_mlcnet_client(self, host):
+        # init MLCnet client
         self.mlcnet_client = SSHClientOverProxy(username=self.username,
                                                 password=self.password,
                                                 proxy_host=self.proxy_host,
-                                                host=host)
+                                                host=self.protocol_host[protocol])
         self.mlcnet_ssh = self.mlcnet_client.get_client()
 
-    def __start_iperf3_server(self):
-        self.mlcnet_ssh.exec_command("nohup iperf3 -s -p {} > /dev/null 2>&1 &".format(self.iperf_server_port))
+    # Helpers Start #
 
-    def __start_tcpdump(self, pcap_filename):
-        stdin, stdout, stderr = self.mlcnet_ssh.exec_command(
-            "sudo -S tcpdump -i ens2 -s 96 src port {} -w {}".format(self.iperf_server_port, pcap_filename),
-            get_pty=True)
+    def sudo(self, ssh, command):
+        stdin, stdout, stderr = ssh.exec_command(f"sudo -S {command}", get_pty=True)
         stdin.write(f'{self.password}\n')
         stdin.flush()
+        return stdin, stdout, stderr
 
-    def __start_iperf3_client(self, server):
+    # Helpers End #
+
+    # Trial Procedure Functions Start #
+
+    def _start_iperf3_server(self):
+        self.mlcnet_ssh.exec_command("nohup iperf3 -s -p {} > /dev/null 2>&1 &".format(self.iperf_server_port))
+
+    def _start_tcpdump(self, pcap_filename):
+        self.sudo(self.mlcnet_ssh,
+                  "tcpdump -i ens2 -s 96 src port {} -w {}".format(self.iperf_server_port, pcap_filename))
+
+    def _start_iperf3_client(self, server):
         if self.iperf3_data is not None:
             command = "iperf3 -R -c {} -p {} -n{}".format(server, self.iperf_server_port, self.iperf3_data)
         else:
@@ -60,11 +68,11 @@ class Trial:
         for line in iter(lambda: stdout.readline(1024), ""):
             logger.debug(line.replace("\n", ""))
 
-    def __shutdown_tasks(self):
-        self.mlcnet_ssh.exec_command('killall tcpdump')
-        self.mlcnet_ssh.exec_command('killall iperf3')
+    def _shutdown_tasks(self):
+        self.sudo(self.mlcnet_ssh, "killall tcpdump")
+        self.mlcnet_ssh.exec_command("killall iperf3")
 
-    def __pcap_to_csv(self, pcap_filename, csv_filename):
+    def _pcap_to_csv(self, pcap_filename, csv_filename):
         command = f'tshark -r ~/{pcap_filename} -T fields \
                                     -e frame.number \
                                     -e frame.len \
@@ -81,46 +89,45 @@ class Trial:
                                     > ~/{csv_filename}'
         self.mlcnet_ssh.exec_command(command)
 
-    def __download_mlcnet_file(self, remote_file, download_to):
+    def _download_mlcnet_file(self, remote_file, download_to):
         sftp = self.mlcnet_ssh.open_sftp()
         sftp.get(remote_file, download_to)
         sftp.close()
 
     # Trial Procedure Functions End #
 
-    def __run_a_trial(self, protocol):
-        logger.info("Started trial for " + protocol)
-
-        logger.info("Connecting to MLCnet server...")
-        self.__init_mlcnet_client(self.protocol_host[protocol])
+    def _run_a_trial(self):
+        logger.info("Started trial for " + self.protocol)
+        output_name = f"{self.protocol}-{int(time.time())}"
+        self.output_names.append(output_name)
 
         # start iperf3 server on MLCnet
         logger.info(f"Starting iperf3 server on port {self.iperf_server_port}")
-        self.__start_iperf3_server()
+        self._start_iperf3_server()
 
         # start tcpdump on server
-        logger.info(f"Starting tcpdump on {self.protocol_host[protocol]}")
-        pcap_filename = self.output_name + ".pcap"
-        self.__start_tcpdump(pcap_filename)
+        logger.info(f"Starting tcpdump on {self.protocol_host[self.protocol]}")
+        pcap_filename = output_name + ".pcap"
+        self._start_tcpdump(pcap_filename)
 
         # run iperf3 client on glomma
         logger.info("Executing iperf3 on client")
-        self.__start_iperf3_client(self.protocol_host[protocol])
+        self._start_iperf3_client(self.protocol_host[self.protocol])
 
         # shutdown tcpdump and iperf3 server
         logger.info("Stopping tcpdump and iperf3...")
-        self.__shutdown_tasks()
+        self._shutdown_tasks()
 
         # export pcap to csv
         logger.info("Exporting pcap data to csv...")
-        csv_filename = self.output_name + ".csv"
-        self.__pcap_to_csv(pcap_filename, csv_filename)
+        csv_filename = output_name + ".csv"
+        self._pcap_to_csv(pcap_filename, csv_filename)
 
         # download csv
-        logger.info("Downloading the csv file...")
-        self.__download_mlcnet_file(f"/home/{self.username}/{csv_filename}",
-                                    f"{os.getcwd()}/csv_files/{csv_filename}")
-        logger.info(f"CSV saved to csv_files/{csv_filename}")
+        logger.info("Downloading csv...")
+        self._download_mlcnet_file(f"/home/{self.username}/{csv_filename}",
+                                   f"{os.getcwd()}/csv_files/{csv_filename}")
+        logger.info(f"File saved to csv_files/{csv_filename}")
 
         # clear temp files on mlcnet server
         logger.info("Cleaning files on servers...")
@@ -128,16 +135,50 @@ class Trial:
 
         logger.info("Trial done.")
 
-        self.mlcnet_client.close()
-
     def run(self):
-        self.__run_a_trial(self.protocol)
+        self._run_a_trial()
 
-    def get_name(self):
-        return self.output_name
+    def get_names(self):
+        return self.output_names
 
     def __del__(self):
+        self.mlcnet_client.close()
         self.glomma_ssh.close()
+
+
+# TCP RWIN comparison
+class RwinTrial(Trial):
+    def _global_init(self):
+        self.sudo(self.mlcnet_ssh, "sysctl net.ipv4.tcp_mem=60000000 60000000 60000000")
+        self.sudo(self.mlcnet_ssh, "sysctl net.ipv4.tcp_wmem=60000000 60000000 60000000")
+        self.sudo(self.mlcnet_ssh, "sysctl net.ipv4.tcp_rmem=60000000 60000000 60000000")
+
+    def _set_default_sysctl_settings(self):
+        self.sudo(self.mlcnet_ssh, "sysctl net.core.rmem_max=212992")
+        self.sudo(self.mlcnet_ssh, "sysctl net.core.wmem_max=212992")
+        self.sudo(self.mlcnet_ssh, "sysctl net.core.rmem_default=212992")
+        self.sudo(self.mlcnet_ssh, "sysctl net.core.wmem_default=212992")
+
+    def _set_higher_rwin(self):
+        self.sudo(self.mlcnet_ssh, "sysctl net.core.rmem_max=60000000")
+        self.sudo(self.mlcnet_ssh, "sysctl net.core.wmem_max=60000000")
+        self.sudo(self.mlcnet_ssh, "sysctl net.core.rmem_default=60000000")
+        self.sudo(self.mlcnet_ssh, "sysctl net.core.wmem_default=60000000")
+
+    def run(self):
+        self._global_init()
+
+        # Run with default rwin settings
+        logger.info("Running with default rwin settings")
+        self._set_default_sysctl_settings()
+        self._run_a_trial()
+
+        # Run with rwin bigger
+        logger.info("Running with higher rwin settings")
+        self._set_higher_rwin()
+        self._run_a_trial()
+
+        self._set_default_sysctl_settings()
 
 
 def main():
@@ -145,10 +186,14 @@ def main():
     password = ""
     protocol = "cubic"
 
-    trial = Trial(protocol, username, password, iperf_time=60)
-    trial.run()
+    rwin_trial = RwinTrial(protocol, username, password, iperf_data="1G")
+    rwin_trial.run()
 
-    p = PlotOneProtocol(protocol, f"{os.getcwd()}/csv_files/{trial.get_name()}.csv", trial.get_name())
+    p = PlotComparison(protocol,
+                       ["default rwin", "higher rwin"],
+                       [f"{os.getcwd()}/csv_files/{rwin_trial.get_names()[0]}.csv",
+                        f"{os.getcwd()}/csv_files/{rwin_trial.get_names()[1]}.csv"],
+                       rwin_trial.get_names()[0])
     p.plot_tput_vs_time()
 
 
