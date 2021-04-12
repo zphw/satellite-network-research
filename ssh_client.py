84ce6ff9e512f1e643c80ea4d3ada8965b8009bc
zphw <zph@zph.io>
2021-04-12 14:36:31 +0800

    optimize ssh connection

--- ssh_client.py
+++ ssh_client.py
@@ -12,34 +12,23 @@ class SSHClientOverProxy:
 
         self.proxy_client = paramiko.SSHClient()
         self.client = paramiko.SSHClient()
+        self.proxy_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     def get_client(self):
         logger.debug("Connecting to proxy " + self.proxy_host)
 
-        self.proxy_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.proxy_client.connect(self.proxy_host, 22, self.username, self.password)
 
         logger.debug("Connected to proxy " + self.proxy_host)
 
-        # get proxy host IP address
-        stdin, stdout, stderr = self.proxy_client.exec_command("hostname -I")
-        proxy_host_addr = stdout.readlines()[0].replace("\n", "")
-
-        # get destination host IP address
-        stdin, stdout, stderr = self.proxy_client.exec_command("resolveip -s " + self.host)
-        host_addr = stdout.readlines()[0].replace("\n", "")
-
-        # logger.debug("Proxy host address: " + proxy_host_addr)
-        # logger.debug("Dest. host address: " + host_addr)
-
         transport = self.proxy_client.get_transport()
-        dest_addr = (host_addr, 22)
-        local_addr = (proxy_host_addr, 22)
-        channel = transport.open_channel("direct-tcpip", dest_addr, local_addr)
+        channel = transport.open_channel("direct-tcpip",
+                                         dest_addr=(self.host, 22),
+                                         src_addr=("", 0))
 
         logger.debug("Connecting to " + self.host)
 
-        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.client.connect(self.host, 22, self.username, self.password, sock=channel)
 
         logger.debug("Connected to " + self.host)
